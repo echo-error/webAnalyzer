1e7d2b4606e29710b12aed2dac23556ee4a09477
echo-error <echo.error@gmail.com>
2018-07-20 17:06:53 +0800

    version 1.0

--- webAnalyzer.py
+++ webAnalyzer.py
@@ -1,7 +1,5 @@
-
 #coding:utf-8
 import os
-import sys
 import pycurl
 import xlsxwriter
 
@@ -16,7 +14,7 @@ c.setopt(pycurl.FORBID_REUSE,1)
 c.setopt(pycurl.MAXREDIRS,1)
 c.setopt(pycurl.MAXREDIRS,1)
 c.setopt(pycurl.DNS_CACHE_TIMEOUT,30)
-indexfile = open(os.path.dirname(os.path.realpath(__file__))+"/content.txt","wb")
+indexfile = open(os.path.dirname(os.path.realpath(__file__))+"\content.txt","wb")
 c.setopt(pycurl.WRITEHEADER,indexfile)
 c.setopt(pycurl.WRITEDATA,indexfile)
 c.perform()
@@ -41,11 +39,11 @@ c.close()
 f = open(os.getcwd()+"/chart.txt",'a')
 f.write(str(HTTP_CODE)+','+str(NAMELOOKUP_TIME * 1000)+','
         +str(CONNECT_TIME * 1000)+','+str(TOTAL_TIME * 1000)+','
-        +str(SIZE_DOWNLOAD/1024)+','+str(HEADER_SIZE)+','
+        +str(SIZE_DOWNLOAD)+','+str(HEADER_SIZE)+','
         +str(SPEED_DOWNLOAD/1024)+'\n')
 f.close()
 
-workbook = xlsxwriter.Workbook(os.getcwd()+"/chart.xlsx")
+workbook = xlsxwriter.Workbook(os.getcwd()+"\chart.xlsx")
 worksheet = workbook.add_worksheet()
 chart = workbook.add_chart({'type':'column'})
 title =  [ URL,
@@ -55,15 +53,62 @@ title =  [ URL,
            u'传输结束总用时',
            u'下载数据包大小',
            u'HTTP头部大小',
-           u'下载速度'
-           ]
+           u'下载速度' ]
+
 format = workbook.add_format()
-format.set_bold(1)
+format.set_border(1)
+format.set_align('center')
+
+format_title = workbook.add_format()
+format.set_border(1)
+
+format_title.set_bg_color('#00F00')
+format_title.set_align('center')
+format_title.set_bold()
+
+worksheet.write_row(0,0,title,format_title)
 
 
 
+f = open('chart.txt','r')
+line = 1
+for i in f:
+    head = [line]
+    linelist = i.split(',') ####chart.txt中的一行转为list型
+    linelist = map(lambda mapi:int(float(mapi.replace("\n",''))),linelist) ####去掉每行中的换行符并整数化
+    linelist = head + linelist
+    worksheet.write_row(line,0,linelist,format)
+    line = line + 1
 
+average = [u'平均值',
+           '=AVERAGE(B2:B' + str(line - 1) + ')',
+           '=AVERAGE(C2:C' + str(line - 1) + ')',
+           '=AVERAGE(D2:D' + str(line - 1) + ')',
+           '=AVERAGE(E2:E' + str(line - 1) + ')',
+           '=AVERAGE(F2:F' + str(line - 1) + ')',
+           '=AVERAGE(G2:G' + str(line - 1) + ')',
+           '=AVERAGE(H2:H' + str(line - 1) + ')',
+           ]
+worksheet.write_row(line,0,average,format)
+f.close()
 
+def chart_series(cur_row):
+    chart.add_series(
+        {
+            'categories': '=Sheet1!$B$1:$H$1',
+            'values'    : '=Sheet1!$B$'+cur_row+':$H$'+cur_row,
+            'line'    : {'color':'black'}, ####图表柱边线颜色
+            'name'      : '=Sheet1!$A$'+cur_row,
 
+        }
+    )
+for row in range(2,line+1):
+    chart_series(str(row))
+
+chart.set_size({'width':876,'height':287})
+
+worksheet.insert_chart(line + 7,0,chart)
+
+workbook.close()
 
 
